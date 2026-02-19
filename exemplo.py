d824d0fd12eeb125bd40e355face02407bb7f2e1
knzzinzx <ana.sanches.freitas@escola.pr.gov.br>
2026-02-19 08:17:26 -0300

    ajsdnf

--- exemplo.py
+++ exemplo.py
@@ -1,4 +1,12 @@
 print("olá mundo")
 import this
-print (7+4)
-print ( "7" + "4")
+
+
+nome= input ('Qual é o seu nome')
+
+idade= input('Qual é a sua idade')
+
+peso= input ('Qual é seu peso')
+
+print(nome,idade,peso )
+
