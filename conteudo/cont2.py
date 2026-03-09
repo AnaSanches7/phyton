17efa458cfd58eb326833b9db3218ea664f89b84
knzzinzx <ana.sanches.freitas@escola.pr.gov.br>
2026-03-09 11:47:28 -0300

    opa

--- conteudo/cont2.py
+++ conteudo/cont2.py
@@ -16,24 +16,35 @@
 
 # == = resultado
 
-print (5+2)
+#print (5+2)
 
-print (5-2)
+#print (5-2)
 
-print (5*2)
+#print (5*2)
 
-print (5/2)
+#print (5/2)
 
-print (5**2)
+#print (5**2)
 
-print (5//2)
+#print (5//2)
 
-print (5%2)
+#print (5%2)
 
-print (5== (2.5*2))
+#print (5== (2.5*2))
 
-# (expressão )
+# (expressão)
 # potenciação
 # multiplicação e divisão 
 #soma e subtração
 
+print (3*5+4**2)
+
+print (3*(5+4)**2)
+
+# pow (x,y) para fazer potenciação
+
+print (pow (9,5))
+
+print (27**(1/3))
+
+print ("="*5)
