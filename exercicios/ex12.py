e37a74c976eccd8ebd55f7d66c8b753c7253d45b
knzzinzx <ana.sanches.freitas@escola.pr.gov.br>
2026-03-16 08:16:59 -0300

    asd

---
+++ exercicios/ex12.py
@@ -0,0 +1,3 @@
+real = float (input("quanto você tem na carteira? R$"))
+dolar = real/5.38
+print('Com R$ {:.2f} você pode comprar US${:.2F}'.format(real,dolar))
---
+++ exercicios/ex13.py
@@ -0,0 +1,3 @@
+largura = float (input("qual a largura da parede: "))
+altura = float (input("qual a altura da parede: "))
+
