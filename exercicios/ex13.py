5633590d876b3c4795fb8435b069606baded8562
knzzinzx <ana.sanches.freitas@escola.pr.gov.br>
2026-03-16 12:23:44 -0300

    qe

--- exercicios/ex13.py
+++ exercicios/ex13.py
@@ -1,3 +1,8 @@
-largura = float (input("qual a largura da parede: "))
-altura = float (input("qual a altura da parede: "))
+largura = float(input('Largura da parede: '))
+altura = float(input('Altura da parede: '))
 
+area = largura * altura
+tinta = area / 2
+
+print(f'Sua parede tem a dimensão de {largura}x{altura} e sua área é de {area}m².')
+print(f'Para pintar essa parede, você precisará de {tinta}l de tinta.')
