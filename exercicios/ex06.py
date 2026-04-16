67241450a6dc6fe57e8cd585f24ff7c0d77f2425
knzzinzx <ana.sanches.freitas@escola.pr.gov.br>
2026-04-16 08:16:49 -0300

    sg

--- exercicios/ex06.py
+++ exercicios/ex06.py
@@ -1,12 +1,2 @@
-entrada = input ("digite algo: ")
-
-eh_letra = (entrada.isalpha())
-
-eh_numero = (entrada.isnumeric())
-
-minusculo = (entrada.islower)
-
-
-print 
-print 
-print 
+cadastro=input('digite algo')
+print()
