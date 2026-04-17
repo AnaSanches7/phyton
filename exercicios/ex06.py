22ba756332da7275ac10aabe818f528708a8f53c
knzzinzx <ana.sanches.freitas@escola.pr.gov.br>
2026-04-17 08:25:28 -0300

    wwe

--- exercicios/ex06.py
+++ exercicios/ex06.py
@@ -1,2 +1,5 @@
-cadastro=input('digite algo')
-print()
+entrada = input("Digite algo: ")
+
+print(f"É letra: {entrada.isalpha()}")
+print(f"É número: {entrada.isdigit()}")
+print(f"É minúscula: {entrada.islower()}")
---
+++ exercicios/ex07.py
@@ -0,0 +1,3 @@
+n = int(input("Digite um número inteiro: "))
+print(f"Antecessor: {n - 1}")
+print(f"Sucessor: {n + 1}")
